b5057dec3e5433ce4d4093ea601346c71545e138
dasha <frgyvu@gmail.com>
2019-11-30 16:20:39 +0300

    tasks 3 for stream API

--- src/lesson17/tasks/task3/Pupil.java
+++ src/lesson17/tasks/task3/Pupil.java
@@ -1,4 +1,21 @@
 package lesson17.tasks.task3;
 
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public class Pupil {
+    enum Gender {
+        MALE, FEMALE
+    }
+    private int number; // уникальное значение для каждого ученика
+    private String name;
+    private Gender gender;
+    private LocalDateTime birth;
+
+    // TODO: добавить все необходимые методы
+
 }
