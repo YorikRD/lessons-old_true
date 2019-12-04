9dbcf752e6f8b487b1376cfb19971765650d3c3b
aud21 <aud@mail.com>
2019-12-04 20:56:09 +0300

    ++

--- src/lesson20/annotations/AnnotationsLesson.java
+++ src/lesson20/annotations/AnnotationsLesson.java
@@ -1,7 +1,9 @@
 package lesson20.annotations;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.Arrays;
 
 @ClassInfo
@@ -14,45 +16,78 @@ public class AnnotationsLesson {
     }
 
     @MethodInfo(author = "ifmo", version = 2)
-    public void someVoid(){
+    public void someVoid() {
         System.out.println("someVoid");
-    }
-
-    @Override
-    @MethodInfo(author = "object")
-    public String toString() {
-        return "AnnotationsLesson{" +
-                "string='" + string + '\'' +
-                '}';
     }
 
     public static void main(String[] args) {
 
-        AnnotationsLesson lesson = new AnnotationsLesson("Lesson");
-        Method[] methods = lesson.getClass().getDeclaredMethods();
-        for (Method method: methods) {
-            Annotation[] annotations = method.getDeclaredAnnotations();
-            System.out.println(Arrays.toString(annotations));
+//        AnnotationsLesson lesson = new AnnotationsLesson("Lesson");
+//        Method[] methods = lesson.getClass().getDeclaredMethods();
+//        for (Method method : methods) {
+//            Annotation[] annotations = method.getDeclaredAnnotations();
+//            System.out.println(Arrays.toString(annotations));
+//
+//            method.isAnnotationPresent(MethodInfo.class);
+//
+//            MethodInfo info = method.getDeclaredAnnotation(MethodInfo.class);
+//            if (info != null) {
+//                System.out.println(info.author());
+//                System.out.println(info.version());
+//            }
+//        }
 
-            method.isAnnotationPresent(MethodInfo.class);
+        Cat cat = new Cat("qqwe", 2, "black");
+        try {
+            System.out.println(toString(cat));
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
 
-            MethodInfo info = method.getDeclaredAnnotation(MethodInfo.class);
-            if (info != null) {
-                System.out.println(info.author());
-                System.out.println(info.version());
+//    static void toString(Cat cat){}
+
+
+    static public String toString(Object o) throws IllegalAccessException {
+        StringBuilder stringBuilder = new StringBuilder("");
+        Field[] fields = o.getClass().getDeclaredFields();
+        for (Field f : fields) {
+            if (f.getType() == String.class) {
+                if (!f.isAnnotationPresent(Exclude.class)) {
+                    if (!f.isAccessible()) f.setAccessible(true);
+                    stringBuilder.append("Поле ")
+                            .append(f.getName())
+                            .append(" имеет значение:")
+                            .append(f.get(o))
+                            .append("\n");
+                }
             }
         }
+        return stringBuilder.toString();
     }
 }
 
 
-//class SomeClass {
-//    аннотация для полей класса
-//    private String name;
-////    private Some some;
-//    private int data;
-//}
+class Cat {
+    private String name;
+    @Exclude
+    private int age;
+    private String color;
+//    private Owner owner;
+
+    public Cat(String name, int age, String color) {
+        this.name = name;
+        this.age = age;
+        this.color = color;
+    }
+}
 
 // написать рефлексивный static toString();
 // "название some : значение"
 // "название data : значение"
+
+// Аннотации:
+// для класса Config(prefix="") получает данные из property файла
+// для класса Init создает
+
+
---
+++ src/lesson20/task/Container.java
@@ -0,0 +1,15 @@
+package lesson20.task;
+
+import java.util.Set;
+
+public class Container {
+    Set<Class> classes;
+
+    public Container(Set<Class> classes) {
+        this.classes = classes;
+    }
+
+    public void init(){
+
+    }
+}
